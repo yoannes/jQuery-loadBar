12026eb94dca0c133b0398ce77bb6238ef6240c7
yoannes <yoannes@gmail.com>
2017-03-01 23:03:47 +0900

    up

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # loadBar
 Small loading bar on top of screen
+
+![alt tag](http://ingate.cc/img/bar.png)
+
+
+# How to use
+Add `<script src="jquery.loadBar.min.js"></script>` before `</body>`
+
+
+## Custom settings
+Custom settings can be defined on `$(document).ready()`
+
+Background and strip color of the bar(default main = red, strip = green):
+```
+loadBar.mainColor = 'COLOR';   // Set the main color of the bar
+loadBar.stripColor = 'COLOR';  // Set the color of the strip
+loadBar.barSpeed = 5;          // Set the speed of the bar
+loadBar.barHeight = 5;         // Set the height of the bar in px
+```
+
+
+## How to use
+```
+loadBar.trigger('show'); // Show the bar
+loadBar.trigger('hide'); // Hide the bar
+```
+
+
+## Example
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>loadBar</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<br /><br />
+<p>Click the buttons below to see it working:</p>
+<button onclick="showBar()">on</button>
+<button onclick="hideBar()">off</button>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="jquery.loadBar.min.js"></script>
+<script>
+   function showBar() {
+      loadBar.trigger('show');
+   }
+   function hideBar() {
+      loadBar.trigger('hide')
+   }
+</script>
+</body>
+</html>
+
+```
---
+++ jquery.loadBar.js
@@ -0,0 +1,99 @@
+/**
+ *
+ * jquery.loadBar.js
+ * @version: v1.0.2
+ * @author: Yoannes Geissler
+ *
+ * Created by Yoannes Geissler on 2017-03-01. Bugs please mail me at yoannes@gmail.com
+ *
+ * Copyright 2017 Yoannes
+ *
+ * The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+
+var loadBar = {
+   init: function () {
+      $('body').append(
+         $("<canvas></canvas>", {
+            "id": "canvasBar",
+            "style": "position: fixed; top: 0; width: 100%; height: "+ this.barHeight +"px; z-index: 9999;"
+         })
+      );
+
+      this.context = document.getElementById("canvasBar").getContext("2d");
+      this.docReady = 1;
+   },
+   trigger: function (action) {
+      if (!this.docReady){ return; }
+
+      if (action == "show") {
+         if (this.barOn){ return; }
+         this.barOn = 1;
+
+         // SHOW CANVAS
+         $("#canvasBar").css("display", "block");
+
+         // BELOW IS OPTIONAL TO DISABLE ALL BUTTONS ON DOCUMENT. COMMENT IF UNNEEDED
+         $(".btn").prop("disabled", true);
+
+         this.run();
+      }
+      else if (action == "hide") {
+         this.barOn = null;
+         $("#canvasBar").css("display", "none");
+      }
+   },
+   run: function () {
+      var grd = loadBar.context.createLinearGradient(loadBar.gradientStartCnt, 0, loadBar.gradientEndCnt, 0);
+      grd.addColorStop(0, loadBar.mainColor);
+      grd.addColorStop(0.5, loadBar.stripColor);
+      grd.addColorStop(1, loadBar.mainColor);
+      loadBar.context.fillStyle = grd;
+      loadBar.context.fillRect(0, 0, 300, 300);
+
+      if (loadBar.gradientStartCnt > 300) {
+         loadBar.gradientEndCnt += loadBar.barSpeed;
+
+         if (loadBar.gradientEndCnt > 300) {
+            loadBar.gradientStartCnt = 0;
+            loadBar.gradientEndCnt = 0;
+         }
+      }else{
+         loadBar.gradientStartCnt += loadBar.barSpeed;
+      }
+
+      if (loadBar.barOn) {
+         setTimeout(loadBar.run, 20);
+      }
+   },
+   context: null,
+   docReady: null,
+   barOn: null,
+   colorStopCnt: 0,
+   gradientStartCnt: 0,
+   gradientEndCnt: 0,
+   mainColor: "red",
+   stripColor: "green",
+   barSpeed: 5,
+   barHeight: 5
+};
+
+$(document).ready(function () {
+   loadBar.init();
+});
